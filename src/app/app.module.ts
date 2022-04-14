df0e2a3d9175240e3cb0717bcbd25c54938442d5
Stefan Watt <stefan.watt@dedalus.eu>
2022-04-14 11:06:23 +0200

    fixed app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
+import { TeamComponent } from './game/team/team.component'
 import { GameComponent } from './game/game.component'
 import { EnemyComponent } from './game/enemy/enemy.component'
 import { MeComponent } from './game/me/me.component'
 import { ControlsComponent } from './game/controls/controls.component'
 import { KpComponent } from './game/kp/kp.component'
+import { MaleIconComponent } from './game/kp/male-icon/male-icon.component'
+import { FemaleIconComponent } from './game/kp/female-icon/female-icon.component'
+import { BarComponent } from './game/kp/bar/bar.component'
 import { StoreModule } from '@ngrx/store'
 import { controlsReducer } from './game/controls/controls.reducer'
-import { GameboyButtonsComponent } from './gameboy-buttons/gameboy-buttons.component';
-import { FooterComponent } from './game/footer/footer.component';
+import { GameboyButtonsComponent } from './gameboy-buttons/gameboy-buttons.component'
+import { FooterComponent } from './game/footer/footer.component'
 import { MessageComponent } from './game/message/message.component'
 
 @NgModule({
   declarations: [
+    BarComponent,
+    MaleIconComponent,
+    FemaleIconComponent,
+    TeamComponent,
     AppComponent,
     GameComponent,
     EnemyComponent,
