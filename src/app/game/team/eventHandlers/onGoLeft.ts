80d1e2486ddfbd351bc5ba7aa639f66a001b76c0
Stefan Watt <stefan.watt@dedalus.eu>
2022-04-20 14:31:34 +0200

    added team eventhandlers

--- src/app/game/team/eventHandlers/onGoLeft.ts
+++ src/app/game/team/eventHandlers/onGoLeft.ts
@@ -1,15 +1,1 @@
-import { controlsStore } from '../controls.store'
-import DIRECTIONS from '../lib/directions'
-import moveCursor from '../lib/moveCursor'
-import { ControlsState } from '../types'
-
-export default () => {
-  controlsStore.update((state: ControlsState) => {
-    const newIndex = moveCursor(state.selectedEntry, DIRECTIONS.LEFT)
-    const selectedEntry =
-      state.entries.find((entry) => entry.id === newIndex) ||
-      state.selectedEntry
-    return { ...state, selectedEntry }
-  })
-}
-
+export default () => {}
--- src/app/game/team/eventHandlers/onGoRight.ts
+++ src/app/game/team/eventHandlers/onGoRight.ts
@@ -1,15 +1,1 @@
-import DIRECTIONS from '../lib/directions'
-import moveCursor from '../lib/moveCursor'
-import { ControlsState } from '../types'
-import { controlsStore } from '../controls.store'
-
-export default () => {
-  controlsStore.update((state: ControlsState) => {
-    const newIndex = moveCursor(state.selectedEntry, DIRECTIONS.RIGHT)
-    const selectedEntry =
-      state.entries.find((entry) => entry.id === newIndex) ||
-      state.selectedEntry
-    return { ...state, selectedEntry }
-  })
-}
-
+export default () => {}
