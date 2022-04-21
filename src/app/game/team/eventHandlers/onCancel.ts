a113a634d0540ef1fcc482873337d547f931f4d7
Stefan Watt <stefan.watt@dedalus.eu>
2022-04-21 08:54:05 +0200

    fixed a bug where cursor position would persist through view switches

--- src/app/game/team/eventHandlers/onCancel.ts
+++ src/app/game/team/eventHandlers/onCancel.ts
@@ -2,8 +2,12 @@ import eventHandlers from '../../controls/eventHandlers'
 import { gameStore } from '../../game.store'
 import { GameState } from '../../types'
 import VIEWS from '../../views'
+import { TeamState, teamStore } from '../team.store'
 
 export default () => {
+  teamStore.update((state:TeamState)=>{
+    return {...state,selectedIndex:0}
+  })
   gameStore.update((state: GameState) => {
     return {
       ...state,
